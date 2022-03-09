b1a7f381c2f17e0d9822c9b7872c4edb9ee27e28
bacikgoz <berkacikgoz@armut.com>
2022-03-09 11:18:09 +0300

    pre cli update commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonModule } from './elements/button/button.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/questions.md
@@ -0,0 +1,13 @@
+## To Be Researched
+
+<hr>
+
+1 - Should we need a module for each custom element?
+
+2 - How to use in different frameworks ?
+
+3 - Is injector required ?
+
+4 - Since we dropped IE are polyfills still required ?
+
+5 - Lazy loading elements ?
